ff5415f3715f6ea439a5d49078c53265222830a7
HelmutRamirez <145595279+HelmutRamirez@users.noreply.github.com>
2024-09-12 17:04:37 -0500

    imagnes

--- PayMaster/settings.py
+++ PayMaster/settings.py
@@ -162,5 +162,6 @@ EMAIL_HOST_PASSWORD = 'krwj apcj sols uowd'  #  contraseña o clave de acceso
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT=BASE_DIR /'media'
-MEDIA_URL='/media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
