6e8542083b69326ca93ae1031977665a392986fb
HelmutRamirez <145595279+HelmutRamirez@users.noreply.github.com>
2024-09-15 22:00:36 -0500

    empresa arreglo url

--- PayMaster/settings.py
+++ PayMaster/settings.py
@@ -102,7 +102,7 @@ else:
         'NAME': 'paymaster',  
         'USER': 'root',                 
         'PASSWORD': '',                         
-        'HOST': '127.0.0.1',                    
+        'HOST': 'localhost',                    
         'PORT': '3306',                          
     }
 }
